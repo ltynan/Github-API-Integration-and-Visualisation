db84bc5cebc41482ae0a86e562a7bcc6c537ca08
ltynan <tynanlu@tcd.ie>
2017-12-12 20:06:27 +0000

    Beginning to store data for graph

--- Github Access/src/GithubAccess.java
+++ Github Access/src/GithubAccess.java
@@ -1,51 +1,70 @@
 import java.util.Scanner;
 import java.io.*;
 
+import org.eclipse.egit.github.core.Commit;
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.User;
-import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.CommitService;
+//import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.eclipse.egit.github.core.service.UserService;
 
 public class GithubAccess {
 
 	public static void main(String[] args) throws IOException {
-		System.out.print("Enter your username:\n");
+		System.out.print("Enter a username:\n");
 		Scanner input = new Scanner (System.in);
 		String owner = input.next();
-		System.out.print("Enter your password:\n");
-		String password = input.next();
 		input.close();
+
+		int repos = noOfPublicRepos(owner);
+		int followers = noOfFollowers(owner);
+		int following = noFollowing(owner);
+		System.out.print("Followers: " + followers + "\nFollowing: " + following + "\nPublic Repos: " + repos);
 		
-		User user = FindUser(owner, password);
-		System.out.println("Information on this user:" +
-							"\nLogin: " + user.getLogin() + 
-							"\nName: " + user.getEmail() + 
-							"\nFollowers: " + user.getFollowers() + 
-							"\nFollowing: "+ user.getFollowing() + 
-							"\nPublic Repos: " + user.getPublicRepos() + 
-							"\nCollaborators: " + user.getCollaborators());
-		
-		printRepos(owner);
-		//String JsonData = toJson(user);
+		FileWriter csv = new FileWriter("src// visualise.csv", false);
+		csv.append("Owner, Followers, Following, No of Repos\n");
+		csv.append(owner+",");
+		csv.append(String.valueOf(followers) +",");
+		csv.append(String.valueOf(following)+",");
+		csv.append(String.valueOf(repos));
+		csv.flush();
+		csv.close();
 	}
 
 	/**
 	 * Print a user's repositories
 	 * @throws IOException 
 	 */
-	private static void printRepos (String owner) throws IOException {
+	private static int noOfPublicRepos (String owner) throws IOException {
+		//FileWriter csv = new FileWriter("src// visualise.csv", false);
 		int count = 1;
 		RepositoryService service = new RepositoryService();
 		for (Repository repo : service.getRepositories(owner)){
-			System.out.println(count++ +") Name: " + repo.getName() + " created on, " + repo.getCreatedAt());
+			System.out.println(count++ +") Name: " + repo.getName());
+			//csv.append(repo.getName());
 		}
+		return --count;
 	}
 	
-	private static User FindUser(String username, String password) throws IOException {
-		GitHubClient client = new GitHubClient();
-		client.setCredentials(username, password);
-		UserService userService = new UserService(client);
-		return userService.getUser();
+
+	private static int noOfFollowers (String username) throws IOException {
+		FileWriter csv = new FileWriter("src// visualise.csv", false);
+
+		int count =0;
+		UserService uService = new UserService();
+		for (User user : uService.getFollowers(username)){
+			count++;
+		}
+		return count;
+	}
+	
+	private static int noFollowing (String username) throws IOException {
+		int count =0;
+		UserService uService = new UserService();
+		for (User user : uService.getFollowing(username)){
+			count++;
+		}
+		return count;
 	}
 }
