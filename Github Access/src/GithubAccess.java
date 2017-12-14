eec3851c66784ec7796a13c85ac085f9ee94755b
ltynan <tynanlu@tcd.ie>
2017-12-14 13:45:31 +0000

    Retrieve and display data regarding the logged in developer
    Data retrieved via authentication as requested in the Description for
    this task.
    Data displayed in the console

--- Github Access/src/GithubAccess.java
+++ Github Access/src/GithubAccess.java
@@ -1,35 +1,54 @@
 import java.util.Scanner;
 import java.io.*;
 
-import org.eclipse.egit.github.core.Commit;
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.User;
-import org.eclipse.egit.github.core.service.CommitService;
-//import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.RepositoryService;
 import org.eclipse.egit.github.core.service.UserService;
+
+//
 
 public class GithubAccess {
 
 	public static void main(String[] args) throws IOException {
-		System.out.print("Enter a username:\n");
+		System.out.println("Would you like to display data via login (0) or use another users data (1)? (0/1)");
 		Scanner input = new Scanner (System.in);
+		int answer = input.nextInt();
+		System.out.print("Enter a username:\n");
 		String owner = input.next();
-		input.close();
 
-		int repos = noOfPublicRepos(owner);
-		int followers = noOfFollowers(owner);
-		int following = noFollowing(owner);
-		System.out.print("Followers: " + followers + "\nFollowing: " + following + "\nPublic Repos: " + repos);
-		
-		FileWriter csv = new FileWriter("src// visualise.csv", false);
-		csv.append("Owner, Followers, Following, No of Repos\n");
-		csv.append(owner+",");
-		csv.append(String.valueOf(followers) +",");
-		csv.append(String.valueOf(following)+",");
-		csv.append(String.valueOf(repos));
-		csv.flush();
-		csv.close();
+		if(answer == 0){
+			System.out.println("Enter your password:");
+			String password = input.next();
+			User user = FindUser(owner, password);
+			System.out.println("Information on authenticated user:"
+								+ "\nLogin: "+ user.getLogin() + 
+								"\nName: " + user.getName() + 
+								"\nEmail: " + user.getEmail() + 
+								"\nFollowers: "+ user.getFollowers() + 
+								"\nFollowing: "+ user.getFollowing() + 
+								"\nPublic Repos: "+ user.getPublicRepos());
+		}
+		else{
+
+			int repos = noOfPublicRepos(owner);
+			int followers = noOfFollowers(owner);
+			int following = noFollowing(owner);
+			System.out.print("Information on external user:" +
+					"\nFollowers: " + followers + "\nFollowing: " + 
+					following + "\nPublic Repos: " + repos);
+
+			FileWriter csv = new FileWriter("src// visualise.csv", false);
+			csv.append("Owner, Followers, Following, Repos\n");
+			csv.append(owner+",");
+			csv.append(String.valueOf(followers) +",");
+			csv.append(String.valueOf(following)+",");
+			csv.append(String.valueOf(repos));
+			csv.flush();
+			csv.close();
+		}
+		input.close();
 	}
 
 	/**
@@ -46,7 +65,7 @@ public class GithubAccess {
 		}
 		return --count;
 	}
-	
+
 
 	private static int noOfFollowers (String username) throws IOException {
 		FileWriter csv = new FileWriter("src// visualise.csv", false);
@@ -58,7 +77,7 @@ public class GithubAccess {
 		}
 		return count;
 	}
-	
+
 	private static int noFollowing (String username) throws IOException {
 		int count =0;
 		UserService uService = new UserService();
@@ -66,5 +85,12 @@ public class GithubAccess {
 			count++;
 		}
 		return count;
+	}
+
+	private static User FindUser(String gitUser, String authenticate) throws IOException {
+		GitHubClient client = new GitHubClient();
+		client.setCredentials(gitUser, authenticate);
+		UserService userService = new UserService(client);
+		return userService.getUser();
 	}
 }
